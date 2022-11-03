0a7d139dbff6521b14414b5e03aa7cc8f5dee606
burakkaplan1 <burakkaplanwork@gmail.com>
2022-11-03 23:44:12 +0100

    [KAT-1] initial Next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: { appDir: true },
+  images: {
+    domains: [
+      "cloud.funda.nl",
+      "istockphoto.com",
+      "firebasestorage.googleapis.com",
+      "media.istockphoto.com",
+    ],
+  },
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+  productionBrowserSourceMaps: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
